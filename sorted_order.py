3b8f04abffd105a8f7902af2d085b13e0cf85bc3
Joaquin Vasquez <joaquin.vasquez@live.com>
2021-01-02 13:56:07 -0600

    Challenge sorted_order file added

---
+++ sorted_order.py
@@ -0,0 +1,20 @@
+# Write a program that reads integers from the user and stores them in a
+# list. Your program should continue reading values until the user enters 0.
+# Then it should display all of the values entered by the user
+# (except for the 0) in order from smallest to largest, with one value
+# appearing on each line. Use either the sort method or the sorted function
+# to sort the list.
+
+def sorted_order(num):
+    for i in num.sort():
+        print(i)
+
+
+if __name__ == '__main__':
+    num = []
+    while True:
+        num.append(int(input("Please enter an integer number into the list: ")))
+        if num[len(num) - 1] == 0:
+            num.pop()
+            break
+    sorted_order(num)
