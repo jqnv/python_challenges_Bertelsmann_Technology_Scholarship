6bee2b2d2108a200cd4a8ab7e596cf6015d22b6f
Joaquin Vasquez <joaquin.vasquez@live.com>
2021-01-19 23:43:02 -0600

    Challenge tail_file file added

---
+++ tail_file.py
@@ -0,0 +1,20 @@
+# Unix-based operating systems also typically include a tool named tail. It displays the last 10 lines of a file
+# whose name is provided as a command line parameter. Write a Python program that provides the same behavior.
+# Display an appropriate error message if the file requested by the user does not exist or if the command line
+# parameter is omitted...
+# You can use the attached "elements.txt" file if you need a file to work with.
+
+## To run the file at the command line Prompt> python tail_file.py elements.txt
+from sys import argv
+from collections import deque
+
+
+def tail_file():
+    # Get the file name from the command line
+    file_2 = argv[1]
+
+    # Open the file using and alias
+    with open(file_2) as my_file:
+        # Print the content of last 10 lines using deque
+        for line in deque(my_file, 10):
+            print(line)
